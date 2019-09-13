89b0c6fe960e73b0bf5ed1e3edae5664155165a9
Aleksei Proskurnov <proskurn@gmail.com>
2019-09-14 00:27:36 +0300

    removed don't used

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import '../pages/layout.scss'
 import '../components/fonts/fonts.scss';
 import '../components/global/global.scss';
 import '../components/header/header.scss';
-import '../components/plan/plan.scss';
 import '../components/ui-header/ui-header.scss';
 import '../components/ui-color/ui-color.scss';
 import '../components/ui-color-rect/ui-color-rect.scss';
